43486ae0c60f1731519cf4a5060b080bbef702f9
wataguchi <sazanami.27.kaito@gmail.com>
2020-04-21 20:12:51 +0900

    ユーザー管理の実装

--- db/migrate/20200410100436_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :users do |t|
-      t.string :users, null: false
-      t.string :name, null: false
-      t.timestamps null: false
-    end
-  end
-end
