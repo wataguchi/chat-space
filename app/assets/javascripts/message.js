a7afb48c40a91dbdf1b9778b3beaccd30f6cc423
wataguchi <sazanami.27.kaito@gmail.com>
2020-04-27 17:59:33 +0900

    chat-space非同期通信まで

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,65 @@
+$(function(){ 
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+      `<div class="chat-message">
+          <div class="top-message">
+            <div class="top-message__name">
+              ${message.user_name}
+            </div>
+            <div class="top-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="bottom-message">
+            <p class="bottom-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+      `<div class="chat-message">
+          <div class="top-message">
+            <div class="top-message__name">
+              ${message.user_name}
+            </div>
+            <div class="top-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="bottom-message">
+            <p class="bottom-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+$('#new_message').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.main__chat').append(html);
+    $('.main__chat').animate({ scrollTop: $('.main__chat')[0].scrollHeight});
+    $('form')[0].reset();
+    $('.form__submit').prop('disabled', false);
+  })
+  .fail(function(){
+    alert("メッセージ送信に失敗しました");
+  })
+})
+});
